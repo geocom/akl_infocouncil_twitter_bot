e065b1902b4da92b41f04b0d7ca01ddfedfe7939
geoff <geoff@new-host-3.home>
2018-02-11 20:28:05 +1300

    Working Commit

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
 gem 'nokogiri'
-
+gem 'twitter'
 #ruby=ruby-2.4.3
 #ruby-gemset=akl_infocouncil
--- bot.rb
+++ bot.rb
@@ -3,18 +3,27 @@ require "rubygems"
 require 'bundler/setup'
 require 'open-uri'
 Bundler.require
-
+DAY_ENDINGS = ["", "st", "nd", "rd", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th", "th", "st"]
 index = Nokogiri::HTML(open('http://infocouncil.aucklandcouncil.govt.nz/'))
 tweetable_items = []
+
+twitter_keys = File.read("#{Dir.pwd}/twitter_api_keys.txt").split("\n")
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = twitter_keys[0]
+  config.consumer_secret     = twitter_keys[1]
+  config.access_token        = twitter_keys[2]
+  config.access_token_secret = twitter_keys[3]
+end
 
 if index.blank? == false
 	if not Dir.exist?("#{Dir.pwd}/meetings")
 		`mkdir #{Dir.pwd}/meetings`
 	end
-	
+
 	meetings = []
-	
-	index.css('.bpsGridMenuItem').each do |link|
+
+	index.css('.bpsGridMenuItem', '.bpsGridMenuAltItem').each do |link|
 		meetings << [Date.parse(link.css('.bpsGridDate')[0].content), link.css('.bpsGridCommittee')[0].content, link.css('.bpsGridAgenda')[0], link.css('.bpsGridAttachments')[0], link.css('.bpsGridMinutes')[0], link.css('.bpsGridMinutesAttachments')[0]]
 	end
 	#puts meetings
@@ -24,56 +33,50 @@ if index.blank? == false
 		if not Dir.exist?(dated_path)
 			`mkdir #{dated_path}`
 		end
-		
+
 		##Many local board/pannels are in Maori which has some charaters that wont work with file systems.So we need to replace those charaters them.
 		foldername = item[1].downcase.gsub(/[^0-9A-Za-z.\-]/, '_')
-		
+
 		if not File.exist?("#{dated_path}/#{foldername}")
 			`mkdir #{dated_path}/#{foldername}`
 		end
 		##Now that this meeting has been registered we need to tell it what has been uploaded so that we can tweet it out. and save it so that next time we won't tweet the same thing.
-		addendum = false
-		#puts item[2].css("a")
-		if item[2].css("a")[0].content.include?("	Addendum")
-			##As far as I know there can only be an addendum to an aggenda and not minutes. However if this does happen in the future I would be happy to add as required
-			addendum = true
-		end
 		agenda_items = []
-		agenda_attachements = [] 
+		agenda_attachements = []
 		minutes = []
 		minutes_attachements = []
-		
+
 		changes_to_folder = [item[0], item[1]]
-		
+
 		item[2].css("a").each do |item|
 			item_filename = item['href'].split("/").last
 			##Has this file already been scrapped
 			if not File.exist?("#{dated_path}/#{foldername}/#{item_filename}.item")
 				##Write the file so we know its added
 				File.open("#{dated_path}/#{foldername}/#{item_filename}.item", 'w') { |file| file.write("") }
-				
+
 				agenda_items << item
 			end
 		end
-		
+
 		item[3].css("a").each do |item|
 			item_filename = item['href'].split("/").last
 			##Has this file already been scrapped
 			if not File.exist?("#{dated_path}/#{foldername}/#{item_filename}.item")
 				##Write the file so we know its added
 				File.open("#{dated_path}/#{foldername}/#{item_filename}.item", 'w') { |file| file.write("") }
-				
+
 				agenda_attachements << item
 			end
 		end
-		
+
 		item[4].css("a").each do |item|
 			item_filename = item['href'].split("/").last
 			##Has this file already been scrapped
 			if not File.exist?("#{dated_path}/#{foldername}/#{item_filename}.item")
 				##Write the file so we know its added
 				File.open("#{dated_path}/#{foldername}/#{item_filename}.item", 'w') { |file| file.write("") }
-				
+
 				minutes << item
 			end
 		end
@@ -83,7 +86,7 @@ if index.blank? == false
 			if not File.exist?("#{dated_path}/#{foldername}/#{item_filename}.item")
 				##Write the file so we know its added
 				File.open("#{dated_path}/#{foldername}/#{item_filename}.item", 'w') { |file| file.write("") }
-				
+
 				minutes_attachements << item
 			end
 		end
@@ -91,10 +94,94 @@ if index.blank? == false
 		changes_to_folder << agenda_attachements
 		changes_to_folder << minutes
 		changes_to_folder << minutes_attachements
-		
+
 		if agenda_items.count > 0 or agenda_attachements.count > 0 or minutes.count > 0 or minutes_attachements.count > 0
 			tweetable_items << changes_to_folder
 		end
 	end
-	puts tweetable_items
+	 tweetable_items.each do |item|
+		 formatted_tweet = []
+		 hashtag_types_included = []
+		 title_type = []
+		 addendum = ""
+		 if item[2][0].content.include?("Addendum")
+			 addendum = " Addendum"
+			 hashtag_types_included << "#CouncilAgendaAddendum"
+		 else
+			 hashtag_types_included << "#CouncilAgenda"
+		 end
+		 if item[2].count > 0
+			 title_type << "Agenda#{addendum}"
+		 end
+		 if item[3].count > 0
+			 title_type << "Agenda#{addendum} Attachments"
+		 end
+		 if item[4].count > 0
+			 title_type << "Minutes"
+			 hashtag_types_included << "#CouncilMinutes"
+		 end
+		 if item[5].count > 0
+			 title_type << "Minutes Attachements"
+			 hashtag_types_included << "#CouncilMinutes"
+		 end
+		  formatted_tweet << "#{item[1]} #{title_type.join(" & ")} for the #{item[0].mday}#{DAY_ENDINGS[item[0].mday]} of #{item[0].strftime("%B %Y")}"
+		 ##This deals with Agendas
+		 if not item[2].count <= 0
+
+			 item[2].each do |url|
+				if url['href'].include?(".PDF") or url['href'].include?(".pdf")
+			 		formatted_tweet << "Agenda PDF: http://infocouncil.aucklandcouncil.govt.nz/#{url['href'].split("?URL=").last}"
+				else
+					formatted_tweet << "Agenda HTML: http://infocouncil.aucklandcouncil.govt.nz/#{url['href'].split("?URL=").last}"
+				end
+			end
+		 end
+		 ##agenda attachements
+		 if not item[3].count <= 0
+			 item[3].each do |url|
+				 if url['href'].include?(".PDF") or url['href'].include?(".pdf")
+					 formatted_tweet << "Attachments PDF: http://infocouncil.aucklandcouncil.govt.nz/#{url['href'].split("?URL=").last}"
+				 else
+					 formatted_tweet << "Attachments HTML: http://infocouncil.aucklandcouncil.govt.nz/#{url['href'].split("?URL=").last}"
+				 end
+			 end
+		 end
+		 ##Miniutes
+		 if not item[4].count <= 0
+			item[4].each do |url|
+				if url['href'].include?(".PDF") or url['href'].include?(".pdf")
+			 		formatted_tweet << "Minutes PDF: http://infocouncil.aucklandcouncil.govt.nz/#{url['href'].split("?URL=").last}"
+				else
+					formatted_tweet << "Minutes HTML: http://infocouncil.aucklandcouncil.govt.nz/#{url['href'].split("?URL=").last}"
+				end
+			end
+		 end
+		 ##Miniutes attachements
+		 if not item[5].count <= 0
+		 item[5].each do |url|
+			 if url['href'].include?(".PDF") or url['href'].include?(".pdf")
+				 formatted_tweet << "Attachments PDF: http://infocouncil.aucklandcouncil.govt.nz/#{url['href'].split("?URL=").last}"
+			 else
+				 formatted_tweet << "Attachments HTML: http://infocouncil.aucklandcouncil.govt.nz/#{url['href'].split("?URL=").last}"
+			 end
+		 end
+		end
+		 formatted_tweet << "#AucklandCouncil #PublicRecords #{hashtag_types_included.join(" ")}"
+		 puts "##Tweet Starts"
+		 puts formatted_tweet.join("\n")
+		 begin
+			 client.update(formatted_tweet.join("\n"))
+		 rescue => e
+			 if e.message.include?("Tweet needs to be a bit shorter")
+				 begin
+					 client.update((formatted_tweet.first(formatted_tweet.size - 1)).join("\n"))
+				 rescue => e
+					 puts e.message
+				 end
+			 else
+				 puts e.message
+			 end
+		 end
+		 puts "##Tweet Ends"
+	 end
 end
